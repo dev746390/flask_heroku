a3cd22dbb53659e41ee6fb369e7cc2d82d423649
dev746390 <tt46478412@gmail.com>
2020-06-24 15:49:16 +0530

    added ip endpoint -- to get heroku ip

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask import jsonify
+import socket 
+
 app = Flask(__name__)
 
 # two decorators, same function
@@ -14,6 +17,16 @@ def symbol():
 @app.route('/myth.html')
 def myth():
     return render_template('myth.html', the_title='Tiger in Myth and Legend')
+
+@app.route('/ip')
+def ip():
+    hostname = socket.gethostname()    
+    IPAddr = socket.gethostbyname(hostname)
+    d = {
+        "hostname": hostname,
+        "IPAddr": IPAddr,
+    }   
+    return jsonify(d)
 
 if __name__ == '__main__':
     app.run(debug=True)
